5138b86476978d50a325bc8bf3d328e82dc01ab0
Dominic Tarr <dominic.tarr@gmail.com>
2018-12-10 18:06:31 +1300

    better readlink

--- c2wasm.sh
+++ c2wasm.sh
@@ -1,10 +1,22 @@
 #! /usr/bin/env bash
 
-platform=./$(uname -s | tr '[:upper:]' '[:lower:]')
+platform=$(uname -s | tr '[:upper:]' '[:lower:]')
 
-"$platform/dist/bin/clang" \
+# I got a feeling osx doesn't come with readlink.
+# thanks: https://stackoverflow.com/a/17577143
+
+function readlink_crossplatform() {
+  (
+  cd $(dirname $1)         # or  cd ${1%/*}
+  echo $PWD/$(basename $1) # or  echo $PWD/${1##*/}
+  )
+}
+
+dir=$(dirname $(readlink_crossplatform $BASH_SOURCE))
+
+"$dir/$platform/dist/bin/clang" \
   --target=wasm32-unknown-unknown-wasm \
-  --sysroot="$platform/sysroot" \
+  --sysroot="$dir/$platform/sysroot" \
   -nostartfiles -Wl,--no-entry,--allow-undefined \
   "$@"
 
